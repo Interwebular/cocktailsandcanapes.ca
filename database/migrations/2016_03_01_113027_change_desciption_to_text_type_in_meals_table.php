969a54e7c48a1b4191d152aabd4a5008b9c7e95b
Brant Wladichuk <brant@interwebular.net>
2016-03-01 03:31:30 -0800

    Update table name

---
+++ database/migrations/2016_03_01_113027_change_desciption_to_text_type_in_meals_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeDesciptionToTextTypeInMealsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('meals', function (Blueprint $table) {
+            $table->text('description')->nullable()->after('name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('meals', function (Blueprint $table) {
+            $table->dropColumn('description');
+        });
+    }
+}
