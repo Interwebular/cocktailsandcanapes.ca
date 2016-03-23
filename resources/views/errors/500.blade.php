53dc3b04e2675b2e2b13e327045ac1b13f26bcea
Brant Wladichuk <brant@interwebular.net>
2016-03-23 02:02:34 -0700

    Add error pages

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -1,34 +1,13 @@
-@extends('layouts.app')
+@extends('errors.template')
 
-@section('htmlheader_title')
-    Server error
-@endsection
-
-@section('contentheader_title')
-    500 Error Page
-@endsection
-
-@section('$contentheader_description')
-@endsection
-
-@section('main-content')
-
-    <div class="error-page">
-        <h2 class="headline text-red">500</h2>
-        <div class="error-content">
-            <h3><i class="fa fa-warning text-red"></i> Oops! Something went wrong.</h3>
-            <p>
-                We will work on fixing that right away.
-                Meanwhile, you may <a href='{{ url('/home') }}'>return to dashboard</a> or try using the search form.
-            </p>
-            <form class='search-form'>
-                <div class='input-group'>
-                    <input type="text" name="search" class='form-control' placeholder="Search"/>
-                    <div class="input-group-btn">
-                        <button type="submit" name="submit" class="btn btn-danger btn-flat"><i class="fa fa-search"></i></button>
-                    </div>
-                </div><!-- /.input-group -->
-            </form>
+@section('content')
+        <div class="container">
+            <div class="col-md-6">
+                <h1>503</h1>
+            </div>
+            <div class="col-md-6">
+                <p>Whoops! Something broke on our side, we will get it fixed asap.</p>
+                <p>In the mean time, you can contact us at 604.424.8788 or info@cocktailsandcanapes.ca</p>
+            </div>
         </div>
-    </div><!-- /.error-page -->
 @endsection
---
+++ resources/views/errors/template.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Whoops! | Cocktails &amp; Canapes</title>
+        <script src="https://use.typekit.net/ahl2ncy.js"></script>
+        <script>try{Typekit.load({ async: true });}catch(e){}</script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+
+        <style>
+            body {
+                font-family: "museo-sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+                background: #262626;
+                color: #fff;
+                padding-top: 200px;
+            }
+
+            h1 {
+                font-size: 10em;
+            }
+        </style>
+    </head>
+
+    <body>
+        @yield('content')
+    </body>
+</html>
